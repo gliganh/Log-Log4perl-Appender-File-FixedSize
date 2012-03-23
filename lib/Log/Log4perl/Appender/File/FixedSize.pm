bc06e84cc62875c459ed98231b0fa4c87b3007ea
Gligan Horea <horea.gligan@evozon.com>
2012-03-23 14:58:38 +0200

    Initial commit

---
+++ lib/Log/Log4perl/Appender/File/FixedSize.pm
@@ -0,0 +1,111 @@
+package Log::Log4perl::Appender::File::FixedSize;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Log::Log4perl::Appender::File::FixedSize - The great new Log::Log4perl::Appender::File::FixedSize!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Log::Log4perl::Appender::File::FixedSize;
+
+    my $foo = Log::Log4perl::Appender::File::FixedSize->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Gligan Calin Horea, C<< <gliganh at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-log-log4perl-appender-file-fixedsize at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Log-Log4perl-Appender-File-FixedSize>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Log::Log4perl::Appender::File::FixedSize
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Log4perl-Appender-File-FixedSize>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Log-Log4perl-Appender-File-FixedSize>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Log-Log4perl-Appender-File-FixedSize>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Log-Log4perl-Appender-File-FixedSize/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Gligan Calin Horea.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Log::Log4perl::Appender::File::FixedSize
